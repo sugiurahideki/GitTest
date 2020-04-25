a769e8cb16440734d1be813ab8fd027982a277e3
sugiurahideki <sugiurahideki@outlook.com>
2020-04-25 17:17:43 +0900

    Playercontroller
    script

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("ahho");
     }
 
     // Update is called once per frame
